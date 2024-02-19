ab3c3e392448dcef2da94e7617b207123d308a9c
mahathi0412 <mandelli@pdx.edu>
2024-02-19 13:49:16 -0800

    Made changes to launchers robot

--- src/mahathi_player_sandbox/RobotPlayer.java
+++ src/mahathi_player_sandbox/RobotPlayer.java
@@ -45,6 +45,7 @@ public strictfp class RobotPlayer {
     static RobotController rc;
     static final int EARLY_GAME_END = 500;
     static final int MID_GAME_END = 1500;
+
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * It is like the main function for your robot. If this method returns, the robot dies!
@@ -288,55 +289,6 @@ public strictfp class RobotPlayer {
         }
     }
 }
-
-
-//    static void runLauncher(RobotController rc) throws GameActionException {
-//        RobotInfo target = findTargetPriority(rc);
-//        if (target != null && rc.canAttack(target.location)) {
-//            rc.attack(target.location);
-//            rc.setIndicatorString("Attacking " + target.location);
-//        } else {
-//            Direction dir = directions[rng.nextInt(directions.length)];
-//            if (rc.canMove(dir)) {
-//                rc.move(dir);
-//            }
-//        }
-//    }
-//
-//    private static RobotInfo findTargetPriority(RobotController rc) throws GameActionException {
-//        RobotInfo[] enemies = rc.senseNearbyRobots(rc.getType().actionRadiusSquared, rc.getTeam().opponent());
-//        if (enemies.length == 0) return null;
-//
-//        RobotInfo prioritizedTarget = null;
-//        double highestPriority = Double.MAX_VALUE;
-//        for (RobotInfo enemy : enemies) {
-//            double priority = calculatePriority(enemy, rc.getLocation());
-//            if (priority < highestPriority) {
-//                highestPriority = priority;
-//                prioritizedTarget = enemy;
-//            }
-//        }
-//        return prioritizedTarget;
-//    }
-//
-//    private static double calculatePriority(RobotInfo enemy, MapLocation myLocation) {
-//        double typePriority = getTypePriority(enemy.type);
-//        double healthFactor = 1.0 / (enemy.health + 1); // Lower health = higher priority
-//        double distanceFactor = 1.0 / myLocation.distanceSquaredTo(enemy.location); // Closer = higher priority
-//
-//        return typePriority * healthFactor * distanceFactor;
-//    }
-//
-//    private static int getTypePriority(RobotType type) {
-//        switch (type) {
-//            case CARRIER: return 1;
-//            case AMPLIFIER: return 2;
-//            case LAUNCHER: return 3;
-//            default: return 10;
-//        }
-//    }
-
-
 
 /*
     static void runLauncher(RobotController rc) throws GameActionException {
